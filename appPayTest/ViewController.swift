fbb54baac63d156969e2e1302873bcb18ae22730
chenshaobo <chenshaobo@ulaiber.com>
2017-01-13 17:44:51 +0800

    first commit

--- appPayTest/ViewController.swift
+++ appPayTest/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mchIDTextField: UITextField!
+    @IBOutlet weak var mchKeyTextField: UITextField!
+    @IBOutlet weak var totalFee: UITextField!
+    @IBOutlet weak var notifyUrlTextField: UITextField!
+    @IBOutlet weak var tradeNoTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    func makeXMLBody(){
+        
+    }
 }
 
---
+++ appPayTest/const.swift
@@ -0,0 +1,14 @@
+//
+//  const.swift
+//  appPayTest
+//
+//  Created by lx on 17/1/13.
+//  Copyright © 2017年 lx. All rights reserved.
+//
+
+import Foundation
+
+
+let SERVER_HOST = "http://10.17.2.253:8080"
+
+let WX_APP_PAY = SERVER_HOST + "weixin/orders"
